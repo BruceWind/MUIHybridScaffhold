f3b048d6a736c010c4dfb37b792a983078481c9b
Bruce <hide@hide.com>
2022-11-24 16:00:19 +0800

    [feat]:{showing navigation bar.}

--- app/src/main/java/io/github/brucewind/FullscreenActivity.kt
+++ app/src/main/java/io/github/brucewind/FullscreenActivity.kt
@@ -31,7 +31,7 @@ class FullscreenActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        binding.webviewContainer.postDelayed({
+        binding.root.postDelayed({
             setFullscreen()
         }, UI_ANIMATION_DELAY)
         initWebView()
@@ -49,9 +49,7 @@ class FullscreenActivity : AppCompatActivity() {
                 View.SYSTEM_UI_FLAG_LOW_PROFILE or
                         View.SYSTEM_UI_FLAG_FULLSCREEN or
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         }
     }
     // add webview into layout
